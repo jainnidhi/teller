f210d023beacc20c05c494c0f2d8efee8efaa810
akshay vinchurkar <akshay@eikyoo.com>
2019-05-16 11:34:43 +0530

    first commit from Sublime Merge

--- 404.php
+++ 404.php
@@ -14,16 +14,16 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'teller' ); ?></h1>
-				</header><!-- .page-header -->
+					<h1 class="page-title">
+						<?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'teller' ); ?>
+					</h1>
+				</header>
 
 				<div class="page-content">
 					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'teller' ); ?></p>
 
 					<?php
 						get_search_form();
-
-						// Only show the widget if site has multiple categories.
 						if ( teller_categorized_blog() ) :
 					?>
 
